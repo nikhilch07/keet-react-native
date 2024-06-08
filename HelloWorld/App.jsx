131651854bd4378404c81e0f0bde47e6d9d29d90
nikhilch07 <nikhilch07@gmail.com>
2024-06-07 17:55:36 -0700

    added styles and backgroundColor to App.jsx

--- HelloWorld/App.jsx
+++ HelloWorld/App.jsx
@@ -1,20 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+      ></View>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
